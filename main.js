0bd4928ce58a34a2244f821ae2691a38dbb9fe90
Poorvi1010 <91541629+Poorvi1010@users.noreply.github.com>
2022-04-12 18:12:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload()
+{
+
+}
+function setup()
+{
+canvas=createCanvas(300,300);
+canvas.center();
+}
+function draw()
+{
+
+}
+function Snapshot()
+{
+ save("Lipstick_Filter_Image.png")
+}
