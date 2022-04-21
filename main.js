0869de69ebecc23de3f04b086af3816771efb56f
Poorvi1010 <91541629+Poorvi1010@users.noreply.github.com>
2022-04-21 21:25:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+lipsx=0;
+lipsy=0;
 function preload()
 {
 
@@ -6,6 +8,24 @@ function setup()
 {
 canvas=createCanvas(300,300);
 canvas.center();
+video=createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on("pose",gotPoses)
+}
+function modelLoaded()
+{
+console.log("PoseNet is Initialized")
+}
+function gotPoses(results)
+{
+ if(results.length>0)
+{
+console.log(results)
+lipsx=results[0].pose.lips.x;
+lipsy=results[0].pose.lips.y;
+ }
 }
 function draw()
 {
